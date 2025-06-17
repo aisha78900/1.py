e26606a81a8a378b6c8c8d6f727af58a5656223b
aisha7890 <aishaowais7890@gmail.com>
2025-06-17 13:04:01 -0700

    test

--- chapter 3.py
+++ chapter 3.py
@@ -1,16 +1,16 @@
-name = "Ayesha"
-Myname = name[1:3]
-print (Myname)
+# name = "Ayesha"
+# Myname = name[1:3]
+# print (Myname)
 
-# Neg slicing 
-print (name [-4 : -1])
-# Isko hm positive me kr ke kr skty hen
-# Agr hm isko ulta kren tw minus se start hoga 
+# # Neg slicing 
+# print (name [-4 : -1])
+# # Isko hm positive me kr ke kr skty hen
+# # Agr hm isko ulta kren tw minus se start hoga 
 
-print(name[:4]) #means 0 to 4 
-print(name[1:]) #means 1 to end 
+# print(name[:4]) #means 0 to 4 
+# print(name[1:]) #means 1 to end 
 
-# skip strings
-word = "123456789" 
-print(word [0:8:3])
+# # skip strings
+# word = "123456789" 
+# print(word [0:8:3])
 # isme 1 ko bhi gny gy or 1 ayega hi ayega means index zero
